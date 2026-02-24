b507ec85155f09377a04f3d68a84a11a3d3a6641
Lucas-2013 <soylucasnavarro@gmail.com>
2026-02-24 21:58:10 +0000

    Starting to learn C++

---
+++ C++/practice.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    //integer (whole number)
+    int x = 7;
+    int y = 2;
+    int sum = x + y;
+    //double (number including decimal)
+    double grade = 6.5;
+    double temperature = 5.3;
+    double sum2 = grade + temperature;
+    //single character
+    char grade2 = 'A';
+    //string (sequence text)
+    std::string name = "Lucas navarro";
+    //output
+    std::cout << name << std::endl;
+    std::cout << "I like pizza" << std::endl;
+    std::cout << "I'ts really good";
+    return 0;
+}
