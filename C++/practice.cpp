f4c37aa1171cf30e9c30e7ba2c88fde1a5c98833
Lucas-2013 <soylucasnavarro@gmail.com>
2026-02-25 05:22:44 +0000

    C++

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-
+namespace first{
+      int age = 7;
+}
 int main(){
+    using std::cout;
+    using std::string;
     //integer (whole number)
-    int age = 7;
-    int y = 2;
+    int age = 2;
     //double (number including decimal)
     double grade = 6.5;
     double temperature = 5.3;
     double sum2 = grade + temperature;
     //single character
-    char grade2 = 'A';
+    const char GRADE2 = 'A';
     //string (sequence text)
-    std::string name = "Lucas navarro";
+    string name = "Lucas navarro";
     //boolean (true or false)
     bool student = true;
     //output
-    std::cout << name << std::endl;
-    std::cout << "I like pizza" << std::endl;
-    std::cout << "I'ts really good" << std::endl;
-    std::cout << "I'ts an new student?" << std::endl;
+   cout << first::age << std::endl;
+    cout << "I like pizza" << std::endl;
+    cout << "I'ts really good" << std::endl;
+    cout << "I'ts an new student?" << std::endl;
     return 0;
 }
